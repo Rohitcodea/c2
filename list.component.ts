49bc21702c994c30a6e3e32ee89f22bf66f18cee
Rohitcodea <164143640+Rohitcodea@users.noreply.github.com>
2024-03-26 08:31:22 +0530

    Update list.component.ts

--- list.component.ts
+++ list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {  FormBuilder,  FormGroup, Validators} from '@angular/forms';
 import { ApiService } from '../../api/api.service';
 import { datamodel } from './model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -9,10 +10,38 @@ import { datamodel } from './model';
   styleUrl: './list.component.css'
 })
 export class ListComponent implements OnInit {
-  dataform!:FormGroup;
-  data:undefined| datamodel[];
+//validation for Number
 
-  
+onlyNumericvalue(event: KeyboardEvent): boolean {
+  const charCode = event.key?.charCodeAt(0) || event.which || event.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    return false;
+    alert("Alphhbets are mot allowes ")
+  }
+  return true;
+}
+onPaste(event:ClipboardEvent){
+  const pastedData= event.clipboardData?.getData('text')||'';
+  if(!/^\d+$/.test(pastedData)){
+    event.preventDefault();
+      alert("Alphabet are not allowed")
+  }
+}
+
+
+onPasteLetter(event:ClipboardEvent){
+  const pastedData= event.clipboardData?.getData('text')||'';
+  if(!/^[a-zA-Z]+$/.test(pastedData)){
+    event.preventDefault();
+    alert("Numbers are not alloed in Name")
+  }
+ 
+
+}
+
+  dataform!:FormGroup;
+  data!: datamodel[]; 
+  Countrie:any;
   indianStates = ["",
     "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
     "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir",
@@ -26,7 +55,7 @@ export class ListComponent implements OnInit {
     "France", "Italy", "Canada", "Australia", "Russia",
     "Germany", "United Kingdom", "Mexico", "South Africa", "Spain"
   ];
-  constructor(private formbuilder:FormBuilder, private api:ApiService){}
+  constructor(private formbuilder:FormBuilder, private api:ApiService,private route:Router){}
   ngOnInit(): void {
     
     this.dataform=this.formbuilder.group({
@@ -35,39 +64,30 @@ export class ListComponent implements OnInit {
       email:['',[Validators.required, Validators.email]],
       dob:['',[Validators.required]],
       age:['',[Validators.required]],
-      number:['',[Validators.required, Validators.pattern("[0-9]{10}")]],
-
+      number:['',[Validators.required,Validators.minLength(10)]],
       gender:['',[Validators.required]],
-      currentaddress:['',Validators.required],
-      currentaddress2: ['', Validators.required],
-      city: ['', Validators.required],
-     
+      currentaddress:['',[Validators.required,Validators.maxLength(60)]],
+      currentaddress2: ['',[ Validators.required,Validators.maxLength(60)]],
+      city: ['', [Validators.required,Validators.maxLength(25)]],     
       selectState: ['',[Validators.required]],
       postalCode: ['', [Validators.required,Validators.minLength(6),Validators.maxLength(6)]],
       country:['',[Validators.required]],
       Course:['',[Validators.required]],
       comments:['',[Validators.required]],
-      imageupload:[null,[Validators.required]],
-
-      
-    
-    
-      accept:['false',[Validators.requiredTrue]],
-      
+      imageupload:[null,],   
+      accept:['',[Validators.requiredTrue]],      
     })  
     this.get();   
   }  
-
-  
-
   adddata(data:datamodel){
-    this.api.add(data).subscribe((res=>{
-      this.dataform.reset();
+    this.api.add(data).subscribe((Response=>{
+      this.dataform.reset(); 
+    //  localStorage.setItem()
+      // console.log(Response.id);
       
-
     }))
     this.get();
-  }
+    }
   get() {
     this.api.get().subscribe(res => {
       this.data = res;
@@ -76,14 +96,29 @@ export class ListComponent implements OnInit {
     });
   }
   delete(id:number){
-    this.api.delete(id).subscribe(()=>{
+    this.api.delete(id).subscribe((res)=>{
       this.get();
       console.log(this.get())
+      console.log(res);      
     })
   }
+  getCon(){
+    this.api.getCoumtary().subscribe(response =>{
+      // // this.Countries=response;
+      // console.log(response);
+      
+      // console.log(this.Countries);
+      
+    })
+  }
+
   getImageUrl(imageFile: File): string {
     return '/path/to/your/images/' + imageFile.name;
   }
+  logout(){
+    localStorage.removeItem("user");
+    localStorage.removeItem('access')
 
-
+    this.route.navigate(["/login"]);
+  }
 }
